428294f425b784ea0a17a87539f786a31242720d
davidkarelh <kareldavid9@gmail.com>
2024-06-11 00:00:38 +0700

    Add health check endpoint

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ app = FastAPI()
 
 handler = Mangum(app)
 
+@app.get("/", )
+async def health_check():
+    return "The health check is successful! See the documentation at: /docs"
+
+
 @app.post("/transcript", response_model=PostTranscriptResponse)
 async def post_transcript(req: PostTranscriptRequest):
     status, response = generate_question(req.competence, req.transcript)
